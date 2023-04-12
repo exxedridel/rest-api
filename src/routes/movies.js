ba84ac267c1017d32edb3ee7193b867d929245b1
hevedrios <exxed.ridel@gmail.com>
2023-04-11 23:08:03 -0500

    ADD post new movie validation

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,11 +1,23 @@
 const { Router } = require("express");
 const router = Router();
 
-const movies = require('../example.json')
+const movies = require("../example.json");
 console.log(movies);
 
-router.get('/', (req, res) => {
-   res.json(movies);
+router.get("/", (req, res) => {
+  res.json(movies);
+});
+
+router.post("/", (req, res) => {
+  const { title, director, year, rating } = req.body;
+  if (title && director && year && rating) {
+    const id = movies.length + 1;
+    const newMovie = { ...req.body, id };
+    movies.push(newMovie);
+    res.json(movies);
+  } else {
+    res.status(500).json({ error: "There was an error." });
+  }
 });
 
 module.exports = router;
